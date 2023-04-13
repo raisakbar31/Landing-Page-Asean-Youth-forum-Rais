6ae06bef71e720a7da4075acb29838e217a35c75
RAIS AKBAR SIDIK <rais.akbar3110@gmail.com>
2023-04-13 13:41:29 +0700

    edit loader

--- js/main.js
+++ js/main.js
@@ -30,16 +30,4 @@
 //]]>
 
 // ===========loader===============
-document.onreadystatechange = function() { 
-    if (document.readyState !== "complete") { 
-        document.querySelector( 
-        "body").style.visibility = "hidden"; 
-        document.querySelector( 
-        "#loader").style.visibility = "visible"; 
-    } else { 
-        document.querySelector( 
-        "#loader").style.display = "none"; 
-        document.querySelector( 
-        "body").style.visibility = "visible"; 
-    } 
-}; 
+$(window).load(function() { $("#loading").delay(5000).fadeOut("slow"); } )
