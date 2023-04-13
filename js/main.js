7dfd6c71b7aa0770516aa78316d8b0bf10f25b16
RAIS AKBAR SIDIK <rais.akbar3110@gmail.com>
2023-04-13 13:25:33 +0700

    add loader

--- js/main.js
+++ js/main.js
@@ -28,3 +28,18 @@
         });
     }
 //]]>
+
+// ===========loader===============
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector( 
+        "body").style.visibility = "hidden"; 
+        document.querySelector( 
+        "#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector( 
+        "#loader").style.display = "none"; 
+        document.querySelector( 
+        "body").style.visibility = "visible"; 
+    } 
+}; 
